0338d082c260124d39b39c2236070d02b57a0ccd
zbeb <smashswooty16@gmail.com>
2022-11-16 00:02:53 +0100

    Revert "Gestion stock de chaussures OOP"
    This reverts commit 44da59239ef595c09bb2267db973005934db82f4.

--- Zouin/2021-2022/Gestion stock de chaussures/main.py
+++ Zouin/2021-2022/Gestion stock de chaussures/main.py
@@ -1,108 +1,38 @@
-from tabulate import tabulate
-
-rowNames = ["Nom", "Pointure (EU)", "Prix (€)"]
-colNames = [1, 2, 3, 4, 5, 6, 0]
-
 stock = [["Asics Gel 2000", 42, 119],
-         ["Asics Gel 2000", 39, 119],
-         ["Mizuno Wave Rider", 38, 129],
-         ["Nike Air Zoom", 42, 125],
-         ["Mizuno Wave Plus", 39, 83.40],
-         ["Mizuno Wave Plus", 40, 83.40],
-         ["Mizuno Wave Plus", 41, 83.40],
-         ["Merrell Poseidon", 39, 118.30]]
+        ["Asics Gel 2000", 39, 119],
+        ["Mizuno Wave rider", 38, 129],
+        ["Nike Air Zoom", 42, 125],
+        ["Mizuno Wave plus", 39, 83.40],
+        ["Mizuno Wave plus", 40, 83.40],
+        ["Mizuno Wave plus", 41, 83.40],
+        ["Merrell Poseidon", 39, 118.30]]
 
-menuOptions = [["Afficher les articles pour une pointure"],
-               ["Afficher les articles présents plusieurs fois"],
-               ["Afficher les articles pour chaque pointure"],
-               ["Afficher la pointure la plus présente"],
-               ["Afficher le nombre de fois la pointure la plus présente"],
-               ["Afficher l’article le plus cher"],
-               ["Quitter le programme"]]
+for item in stock:
+    print(f"{item[0]} | {item[1]} | {item[2]}")
 
+print(f"\n 1 : Afficher les articles pour une pointure\n",
+        "2 : Afficher les articles présents plusieurs fois\n",
+        "3 : Afficher les articles pour chaque pointure\n",
+        "4 : Afficher la pointure la plus présente\n",
+        "5 : Afficher le nombre de fois la pointure la plus présente\n",
+        "6 : Afficher l’article le plus cher\n",
+        "0 : Quitter le programme\n")
 
-def show_catalogue():
-    print("Voici le catalogue:")
-    print(f"{tabulate(stock, headers=rowNames, numalign='center', tablefmt='rounded_grid')}\n\n")
-
-
-def show_options():
-    print("Voici les commandes disponibles")
-    print(f"{tabulate(menuOptions, tablefmt='rounded_grid', showindex=colNames)}\n\n")
-
-
-def search_by_size():
-    sizes = []
+def sizeSearch():
     found = False
     size = int(input("Quelle taille voulez vous: "))
+    print() # Adds space between input and print
 
     for i in stock:
         if size in i:
             found = True
-            sizes.append([i[0]])
-    print(f"{tabulate(sizes, headers=rowNames, tablefmt='rounded_grid')}")
+            print(f"{i[0]} | {i[1]} | ${i[2]}")
     if not found:
         print("Cette taille n'est pas dans notre base de donnees")
 
-
-def multiple_articles():
-    shoe_sizes = {}
-    final = []
-    headers = ["Model", "Pointure"]
-
-    for name, *size in stock:  # Unpack the 'stock' list, place first value in name and every other value in size hence the '*'
-        if name in shoe_sizes:  # Checks if the shoe's name is already in the dictionary
-            shoe_sizes[name].append(size[0])  # Append the shoe size to the already existing key in the dictionary
-        else:
-            shoe_sizes[name] = [size[0]]  # If the key doesn't exist add it and asign the size
-
-    for shoe, sizes in shoe_sizes.items():  # Unpack the dictionary containing the shoe name and all sizes available for it
-        if len(sizes) > 1:  # Checks if a key has multiple values eg. "Asics Gel 2000", [42, 39]
-            final.append([shoe, sizes])  # Append the name and the sizes to a final list
-    print(f"{tabulate(final, headers, tablefmt='rounded_grid')}")
-
-
-def size_by_article():
-    show_catalogue()
-
-
-def most_frequent_size():
-    sizes = []
-    for i in stock:
-        sizes.append(i[1])
-    print(f"La pointure la plus présente est: {max(sizes, key=sizes.count)}")
-
-
-def number_of_most_frequent_size():
-    sizes = []
-    for i in stock:
-        sizes.append(i[1])
-    nbrOfMostFrequentSize = max(sizes, key=sizes.count)
-    print(f"La pointure la plus présente apparait {sizes.count(nbrOfMostFrequentSize)} fois")
-
-
-def most_expensive_item():
-    prices = []
-    for i in stock:
-        prices.append(i)
-    prices.sort()
-    prices = [prices[-1]]
-    print(tabulate(prices, headers=rowNames, tablefmt='rounded_grid'))
-
-
-def func_quit():
-    exit()
-
-
-show_catalogue(), show_options()
 options = {
-    "1": search_by_size,
-    "2": multiple_articles,
-    "3": size_by_article,
-    "4": most_frequent_size,
-    "5": number_of_most_frequent_size,
-    "6": most_expensive_item,
-    "0": func_quit
+    1: sizeSearch
 }
-userInput = input("Quelle commande voulez vous: ")
-options[userInput]()
+
+response = int(input("Quelle commande voulez vous: "))
+options[response]()
